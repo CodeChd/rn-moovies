b6a46dd3a2cde7a3eb867ebc22131cd341c853f0
CodeChd <drowzyking1126@gmail.com>
2024-02-14 23:44:44 +0800

    Base

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from "react";
+import { Image, Text, View } from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { useFonts } from "expo-font";
+import { Asset, useAssets } from "expo-asset";
+
+SplashScreen.preventAutoHideAsync();
+
+// const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+// const assets = (assets) =>
+//   assets.map((asset) => {
+//     if (typeof asset === "string") {
+//       return Image.prefetch(asset); //load images from network
+//     } else {
+//       return Asset.loadAsync(asset); //load images from local
+//     }
+//   });
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  //use this hook when you just want to load assets
+  const [assets, error] = useAssets([require("./assets/me.jpg")]); //use this hook to load images from local or network
+  const [fontsLoaded, fontError] = useFonts(Entypo.font); //use this hook to load fonts from local or network
+
+  if (assets && fontsLoaded) {
+    SplashScreen.hideAsync();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>
+        SplashScreen Demo! <Entypo name="rocket" size={20} />
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
